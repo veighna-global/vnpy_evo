53bb2e6a6c4d236d84b3e53832e751185f72db15
VeighNa Global <veighna@hotmail.com>
2024-07-23 22:35:58 +0800

    [Mod] reimplement extract_vt_symbol function

--- vnpy_evo/trader/utility.py
+++ vnpy_evo/trader/utility.py
@@ -1,1 +1,11 @@
 from vnpy.trader.utility import *
+
+from .constant import Exchange
+
+
+def extract_vt_symbol(vt_symbol: str) -> tuple[str, Exchange]:
+    """
+    return (symbol, exchange)
+    """
+    symbol, exchange_str = vt_symbol.rsplit(".", 1)
+    return symbol, Exchange(exchange_str)
