c660502749163af1275f9b60691cdf1460aa9e59
vn.py <xiaoyou.chen@foxmail.com>
2024-08-09 19:54:31 +0800

    [Mod] update global settings

--- vnpy_evo/trader/setting.py
+++ vnpy_evo/trader/setting.py
@@ -1,1 +1,49 @@
-from vnpy.trader.setting import *
+"""
+Global setting of the trading platform.
+"""
+
+from logging import INFO
+from tzlocal import get_localzone_name
+
+from .utility import load_json
+
+
+SETTINGS: dict[str, object] = {
+    "font.family": "",
+    "font.size": 12,
+
+    "log.active": True,
+    "log.level": INFO,
+    "log.console": True,
+    "log.file": True,
+
+    "email.server": "",
+    "email.port": 0,
+    "email.username": "",
+    "email.password": "",
+    "email.sender": "",
+    "email.receiver": "",
+
+    "datafeed.name": "",
+    "datafeed.username": "",
+    "datafeed.password": "",
+
+    "database.timezone": get_localzone_name(),
+    "database.name": "duckdb",
+    "database.database": "database.duckdb",
+    "database.host": "",
+    "database.port": 0,
+    "database.user": "",
+    "database.password": ""
+}
+
+
+# Load global setting from json file.
+SETTING_FILENAME: str = "vt_setting.json"
+SETTINGS.update(load_json(SETTING_FILENAME))
+
+
+def get_settings(prefix: str = "") -> dict[str, object]:
+    prefix_length: int = len(prefix)
+    settings = {k[prefix_length:]: v for k, v in SETTINGS.items() if k.startswith(prefix)}
+    return settings
